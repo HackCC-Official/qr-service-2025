b2756e46e6fbd3461ecb437f4a22c66c56504182
chheangg <balldremz@gmail.com>
2025-02-11 11:03:22 -0800

    add workshop controller

---
+++ src/workshop/workshop.controller.ts
@@ -0,0 +1,63 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { WorkshopService } from "./workshop.service";
+import { ApiBody, ApiOperation, ApiParam } from "@nestjs/swagger";
+import { ResponseWorkshopDTO } from "./response-workshop.dto";
+import { RequestWorkshopDTO } from "./request-workshop.dto";
+
+@Controller('workshops')
+export class WorkshopController {
+  constructor(
+    private workshopService: WorkshopService
+  ) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Finds all Workshops' })
+  async findAll() {
+    return await this.workshopService.findAll()
+  }
+
+  @Get(':workshop_id')
+  @ApiOperation({ summary: 'Finds a single Workshop by workshop_id'})
+  async findById(
+    @Param('workshop_id') id: string
+  ): Promise<ResponseWorkshopDTO> {
+    return await this.workshopService.findById(id);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Creates a new workshop for the event' })
+  @ApiBody({
+    description: 'Workshop object of an actual workshop',
+    type: RequestWorkshopDTO
+  })
+  async create(
+    @Body() createWorkshopDTO: RequestWorkshopDTO
+  ) : Promise<ResponseWorkshopDTO> {
+    return await this.workshopService.create(createWorkshopDTO);
+  }
+
+  @Put(':workshop_id')
+  @ApiOperation({ summary: 'Updates an existing workshop' })
+  @ApiParam({
+    description: 'ID of existing workshop',
+    name: 'workshop_id'
+  })
+  async update(
+    @Param('workshop_id') id: string,
+    @Body() updateWorkshopDTO: RequestWorkshopDTO
+  ) : Promise<ResponseWorkshopDTO> {
+    return await this.workshopService.update(id, updateWorkshopDTO)
+  }
+
+  @Delete(':workshop_id')
+  @ApiOperation({ summary: 'Deletes a workshop' })
+  @ApiParam({
+    description: 'ID of existing workshop',
+    name: 'workshop_id'
+  })
+  async delete(
+    @Param('workshop_id') id: string
+  ) {
+    return this.workshopService.delete(id);
+  }
+}
