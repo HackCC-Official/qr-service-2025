f098b8cf4b0a36c35c0bbe0919c928fa260f70f2
chheangg <balldremz@gmail.com>
2025-04-22 23:41:53 -0700

    fix migration

--- drizzle/0005_outgoing_wolfpack.sql
+++ drizzle/0005_outgoing_wolfpack.sql
@@ -1,4 +1,4 @@
 ALTER TYPE "public"."status" ADD VALUE 'ABSENT';--> statement-breakpoint
 ALTER TYPE "public"."status" ADD VALUE 'ALL';--> statement-breakpoint
-ALTER TABLE "workshop_organizers" ALTER COLUMN "workshop_id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "workshop_organizers" ALTER COLUMN "workshop_id" SET DATA TYPE uuid USING workshop_id::uuid;
 ALTER TABLE "workshop_organizers" ADD CONSTRAINT "workshop_organizers_workshop_id_workshops_id_fk" FOREIGN KEY ("workshop_id") REFERENCES "public"."workshops"("id") ON DELETE no action ON UPDATE no action;
